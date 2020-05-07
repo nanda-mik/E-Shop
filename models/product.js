d8d5e733858db4d910bb7b70d6b2af125431fd09
nanda-mik <nandamikushubha@gmail.com>
2020-05-07 19:36:57 +0530

    initial commit when starting with mongodb

---
+++ models/product.js
@@ -0,0 +1,39 @@
+const mongodb = require('../util/database');
+
+class Product{
+  constructor(title, price, description, imageUrl){
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  save(){
+
+  }
+
+}
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Product;
