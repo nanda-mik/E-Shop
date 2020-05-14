383d832bc0b3df92b9543e57137a877bbba3a9ff
nanda-mik <nandamikushubha@gmail.com>
2020-05-14 10:52:13 +0530

    second commit after creating a user with simple js not authentication

--- models/user.js
+++ models/user.js
@@ -1,16 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class User{
+  constructor(username, email){
+    this.name = username;
+    this.email= email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save(){
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  static findById(userId){
+    const db = getDb();
+    return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)})
+    .then(user => {
+      console.log(user);
+      return user;
+    })
+    .catch(err => console.log(err));
+    // use find().next() and findOne() for one user only. same meaning find().next() returns a cursir pointing to that id but findOne() ives the object .
+  }
+}
 
 module.exports = User;
