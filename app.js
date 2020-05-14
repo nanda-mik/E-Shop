4e979180486be74b4fd2a711be30662dfd28b9b6
nanda-mik <nandamikushubha@gmail.com>
2020-05-14 14:55:15 +0530

    after working with all the operations in mongodb building cart and orders

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById("5ebcce7741bb359c81c7841d")
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name,user.email,user.cart,user._id);
             next();
         })
         .catch(err => console.log(err));
