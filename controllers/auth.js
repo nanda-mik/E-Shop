327ca08b85aabf2c6e09fed1aafb5bdbe0330744
nanda-mik <nandamikushubha@gmail.com>
2020-05-26 17:02:00 +0530

    reset password feature added

--- controllers/auth.js
+++ controllers/auth.js
@@ -216,3 +216,62 @@ exports.postReset = (req, res, next) => {
       });
   });
 };
+
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  const db = getDb();
+  db.collection('users').findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+  const db = getDb();
+
+  db.collection('users').findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+      return db.collection('users').update(
+        {'_id' : new mongodb.ObjectId(userId)},
+        { '$set' : {
+          'password' : hashedPassword,
+          'resetToken': undefined,
+          'resetTokenExpiration': undefined
+        }}
+      )
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
